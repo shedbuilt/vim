52900c5039807031de1630b25e506c053eae5cd4
Auston Stewart <auston@shedbuilt.net>
2018-02-05 15:23:38 -1000

    Updating default vimrc

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
-./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
+./configure --prefix=/usr && \
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
 ln -sv vim "${SHED_FAKEROOT}/usr/bin/vi"
-for L in ${SHED_FAKEROOT}/usr/share/man/{,*/}man1/vim.1; do
+for L in "${SHED_FAKEROOT}"/usr/share/man/{,*/}man1/vim.1; do
     ln -sv vim.1 $(dirname $L)/vi.1
 done
 mkdir -pv "${SHED_FAKEROOT}/usr/share/doc"
