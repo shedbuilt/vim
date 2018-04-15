2fbe8470fa9846d1ca3c8dfe63fc84fb49a7675d
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:29:29 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
 ./configure --prefix=/usr && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-ln -sv vim "${SHED_FAKEROOT}/usr/bin/vi"
-for L in "${SHED_FAKEROOT}"/usr/share/man/{,*/}man1/vim.1; do
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+ln -sv vim "${SHED_FAKE_ROOT}/usr/bin/vi"
+for L in "${SHED_FAKE_ROOT}"/usr/share/man/{,*/}man1/vim.1; do
     ln -sv vim.1 $(dirname $L)/vi.1
 done
-mkdir -pv "${SHED_FAKEROOT}/usr/share/doc"
-ln -sv ../vim/vim80/doc "${SHED_FAKEROOT}/usr/share/doc/vim-8.0.586"
-install -v -Dm644 "${SHED_CONTRIBDIR}/vimrc" "${SHED_FAKEROOT}/etc/vimrc.default"
+mkdir -pv "${SHED_FAKE_ROOT}/usr/share/doc"
+ln -sv ../vim/vim80/doc "${SHED_FAKE_ROOT}/usr/share/doc/vim-8.0.586"
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/vimrc" "${SHED_FAKE_ROOT}/etc/vimrc.default"
